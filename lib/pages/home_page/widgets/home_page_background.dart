4213561ef54b5b2b539f10c5c233effb029dd13a
Can Arslan <canarslanx@gmail.com>
2024-12-27 04:02:08 +0300

    fixed black hole animation bug

--- lib/pages/home_page/widgets/home_page_background.dart
+++ lib/pages/home_page/widgets/home_page_background.dart
@@ -19,21 +19,25 @@ class _Background extends GetView<HomePageController> {
   Widget get vsCodeWallpaper => SizedBox(
       width: 100.w,
       height: 100.h,
-      child: Image.asset('assets/images/wallpaper.png', fit: BoxFit.fitWidth));
+      child: Image.asset('assets/images/wallpaper.png', fit: BoxFit.cover));
 
   Widget get blackHole => Positioned(
-        top: -Get.width * 2,
-        left: -Get.width * 2,
+        top: -Get.height * 4 * (Get.width / Get.height),
+        left: -Get.width * 4,
         child: Obx(
           () => AnimatedContainer(
             margin: EdgeInsets.only(
                 top: !controller.wallpaperAnimation.value
-                    ? Get.height * 4 + Get.height * 0.5
-                    : Get.height,
+                    ? (Get.height * 4 * (Get.width / Get.height)) +
+                        Get.height * 0.5
+                    : (Get.height * 4 * (Get.width / Get.height)) / 2 +
+                        Get.height * 0.5,
                 left: !controller.wallpaperAnimation.value
-                    ? Get.width * 2 + Get.width * 0.5
-                    : Get.width * 0.5),
-            height: !controller.wallpaperAnimation.value ? 0 : Get.width * 4,
+                    ? Get.width * 4 + Get.width * 0.5
+                    : Get.width * 2 + Get.width * 0.5),
+            height: !controller.wallpaperAnimation.value
+                ? 0
+                : Get.height * 4 * (Get.width / Get.height),
             width: !controller.wallpaperAnimation.value ? 0 : Get.width * 4,
             curve: Curves.easeInOut,
             duration: const Duration(seconds: 2),
